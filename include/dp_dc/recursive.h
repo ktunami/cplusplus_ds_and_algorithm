747fa73013a78d7a5409217d267db1dfdcce5bed
ktunami <1506712249@qq.com>
2020-11-02 11:21:40 +0800

    add comment

--- include/dp_dc/recursive.h
+++ include/dp_dc/recursive.h
@@ -12,8 +12,14 @@
 
 class Recursive{
 public:
+  /// @brief Reverse a stack inplace
+  /// @param st : The stack to be operated
   void ReverseStack(std::stack<int> &st);
+
 private:
+  /// @brief Get the bottom element from stack
+  /// @param st : The stack to be operated
+  /// @return The bottom element in the stack
   int get_bottom_num(std::stack<int> &st);
 };
 #endif//C_PRACTICE_RECURSIVE_H
